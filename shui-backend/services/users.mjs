e31998970df1766c9bef3a023cade52ce0a819f6
Felicia-V03 <3kdyhfe24.udomfe@folkuniversitetet.nu>
2025-09-23 21:07:47 +0200

    add get user

--- shui-backend/services/users.mjs
+++ shui-backend/services/users.mjs
@@ -1,6 +1,7 @@
 import { hashPassword } from "../utils/bcrypt.mjs";
-import { PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { PutItemCommand, GetItemCommand } from "@aws-sdk/client-dynamodb";
 import { client } from "./client.mjs";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
 
 export const createUser = async (user) => {
   console.log("Creating user:", user);
@@ -24,6 +25,27 @@ export const createUser = async (user) => {
     return true;
   } catch (error) {
     console.error("Error creating user:", error);
+    return false;
+  }
+}
+
+export const getUser = async (username) => {
+  const command = new GetItemCommand({
+    TableName : 'shui-messages-table',
+    Key : {
+      PK : { S : `USER#${username}` },
+      SK : { S : 'PROFILE' }
+    }
+  });
+
+  try {
+    const { Item } = await client.send(command);
+    if(!Item) return false;
+
+    const user = unmarshall(Item);
+    return user;
+  } catch (error) {
+    console.error("Error fetching user:", error);
     return false;
   }
 }
