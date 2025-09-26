2518abeb9b28f5bc72e867653138a0614d0a4091
Felicia-V03 <3kdyhfe24.udomfe@folkuniversitetet.nu>
2025-09-26 12:24:55 +0200

    create authenticateuser

--- shui-backend/middlewares/validateMessage.mjs
+++ shui-backend/middlewares/validateMessage.mjs
@@ -1,13 +1,11 @@
 import { sendResponse } from "../responses/index.mjs";
 
-export const validateMessage = () => {
-  return {
-    before: async (request) => {
-      const body = request.event.body;
+export const validateMessage = () => ({
+  before: async (handler) => {
+    const body = handler.event.body;
 
-      if (!body || !body.message || typeof body.message !== 'string' || body.message.trim() === '') {
-        throw sendResponse(400, { message: 'Invalid message format' });
-      }
+    if (!body || !body.message || typeof body.message !== 'string' || body.message.trim() === '') {
+      throw sendResponse(400, { message: 'Invalid message format' });
     }
-  };
-};
+  }
+});
