324fb36fb3d8d8191018e12addbe01e4628c0fc2
Felicia-V03 <3kdyhfe24.udomfe@folkuniversitetet.nu>
2025-09-26 12:03:16 +0200

    create message iservices done

--- shui-backend/functions/messages/CreateMessage/index.mjs
+++ shui-backend/functions/messages/CreateMessage/index.mjs
@@ -2,16 +2,20 @@ import middy from '@middy/core'
 import httpJsonBodyParser from '@middy/http-json-body-parser'
 import { sendResponse } from '../../../responses/index.mjs'
 import { errorHandler } from '../../../middlewares/errorHandler.mjs'
-import { createMessage } from '../../../services/messages.mjs'
+import { addMessage } from '../../../services/messages.mjs'
 import { validateMessage } from '../../../middlewares/validateMessage.mjs'
 
 export const handler = middy(async (event) => {
-  const messageData = await createMessage(event.body);
+  const user = event.user;
+  const messageData = await addMessage({
+    ...event.body,
+    userId: user.username 
+  });
 
   if (messageData.seccess) {
     return sendResponse(201, {
       message: 'Message created successfully',
-      data: messageData.data
+      data: messageData
     });
   } else {
     return sendResponse(500, { message: 'Failed to create message' });
