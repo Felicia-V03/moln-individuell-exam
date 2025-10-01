8ded82d014d7cd0624a84ef9717500e3e87292ec
Felicia-V03 <3kdyhfe24.udomfe@folkuniversitetet.nu>
2025-10-01 19:13:10 +0200

    add title

--- shui-backend/models/messageSchema.mjs
+++ shui-backend/models/messageSchema.mjs
@@ -2,5 +2,6 @@ import Joi from 'joi';
 
 // schema för meddelandedata
 export const messageSchema = Joi.object({
+  title: Joi.string().min(1).max(40).required(),
   message: Joi.string().min(1).max(500).required(),
 });
