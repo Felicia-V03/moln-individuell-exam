53edbf6452d8af3ed678aa7e63d92a9730fddea1
Felicia-V03 <3kdyhfe24.udomfe@folkuniversitetet.nu>
2025-10-02 14:19:08 +0200

    done with navmeny

--- shui-frontend/src/pages/MessagesPage/MessagesPage.jsx
+++ shui-frontend/src/pages/MessagesPage/MessagesPage.jsx
@@ -1,12 +1,16 @@
 import './messagesPage.css';
 import { useNavigate } from 'react-router-dom';
 import MessagesList from '../../components/MessagesList/MessagesList';
+import Navigation from '../../components/Navigation/Navigation';
 
 const MessagesPage = () => {
   const navigate = useNavigate();
   return (
     <section className="messages-page">
-      <h1 className="messages-page__title">MESSAGES</h1>
+      <header>
+        < Navigation />
+        <h1 className="messages-page__title">MESSAGES</h1>
+      </header>
       < MessagesList />
     </section>
   )
